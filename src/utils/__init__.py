4ed59ff60ed90bacd423a390d4e5b8fc66d5ddf9
Maiker <gutierrezmaiker@gmail.com>
2026-04-11 01:07:27 -0600

    Add Client Uninstaller

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .load_user_config import load_user_config
-from .tunnel_data import tunnel_data
+from .tunnel_data import tunnel_name
 from .PATHS import CLOUDFLARED_PATH, CLOUDFLARED_DIR, USER_DATA_DIR
 
-__all__ = ["load_user_config", "tunnel_data", "CLOUDFLARED_PATH", "CLOUDFLARED_DIR", "USER_DATA_DIR"]
+__all__ = ["load_user_config", "tunnel_name", "CLOUDFLARED_PATH", "CLOUDFLARED_DIR", "USER_DATA_DIR"]

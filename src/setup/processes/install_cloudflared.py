f1bc535a6f6dc98c1daadb795e10ce9562f4be4a
Maiker <gutierrezmaiker@gmail.com>
2026-04-10 21:09:11 -0600

    Add Client Setup Script

--- src/setup/processes/install_cloudflared.py
+++ src/setup/processes/install_cloudflared.py
@@ -2,4 +2,4 @@ import subprocess
 
 def install_cloudflared():
     print("Installing cloudflared...")
-    subprocess.run("winget install Cloudflare.cloudflared -e --silent", shell=True)
+    subprocess.run("winget install --id Cloudflare.cloudflared --source winget -e --silent", shell=True)

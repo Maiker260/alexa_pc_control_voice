a9fb1759407804ef91b5fd4919e80c7dabb4a9a6
Maiker <gutierrezmaiker@gmail.com>
2026-04-10 20:30:15 -0600

    Add Client Setup Script

---
+++ main_installer.py
@@ -0,0 +1,19 @@
+from setup.gui import run_gui
+from setup.setup import run_setup
+from .src.start_services import start_services
+
+def pipeline(domain):
+    try:
+        run_setup(domain)
+        start_services()
+
+    except Exception as e:
+        print(f"Installation Error: {e}")
+
+
+def main():
+    run_gui(pipeline)
+
+
+if __name__ == "__main__":
+    main()

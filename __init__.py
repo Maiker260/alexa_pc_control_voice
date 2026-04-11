696f743a438eee07b04692841e497af8a7d689b0
Maiker <gutierrezmaiker@gmail.com>
2026-04-11 02:53:56 -0600

    Add Client Uninstaller

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .main_installer import main
-from .uninstaller import uninstaller
-
-__all__ = ["main", "uninstaller"]
--- src/uninstall/uninstall.py
+++ src/uninstall/uninstall.py
@@ -4,8 +4,8 @@ import shutil
 import ctypes
 import sys
 from run_ps import run_ps
-from src.utils.tunnel_data import tunnel_name
-from src.utils.PATHS import CLOUDFLARED_PATH, CLOUDFLARED_DIR, USER_DATA_DIR
+from utils.tunnel_data import tunnel_name
+from utils.PATHS import CLOUDFLARED_PATH, CLOUDFLARED_DIR, USER_DATA_DIR
 
 def is_admin():
     try:

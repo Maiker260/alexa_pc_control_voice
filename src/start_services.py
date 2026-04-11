5ea776eb6751fff4738b9e11e1a4dc165083e764
Maiker <gutierrezmaiker@gmail.com>
2026-04-11 03:59:15 -0600

    Add Client Setup Script

--- src/start_services.py
+++ src/start_services.py
@@ -1,14 +1,14 @@
 import subprocess
 import time
-from .setup.processes.cloudflare_process import CLOUDFLARED
 from .utils.tunnel_data import tunnel_name
+from .utils.PATHS import CLOUDFLARED_PATH
 
 def start_services():
     print("Starting Services...")
 
     subprocess.Popen("uvicorn main:app --host 0.0.0.0 --port 8000", shell=True)
     time.sleep(5)
-    subprocess.Popen(f"{CLOUDFLARED} tunnel run {tunnel_name}", shell=True)
+    subprocess.Popen(f'"{CLOUDFLARED_PATH}" tunnel run {tunnel_name}', shell=True)
 
 if __name__ == "__main__":
     start_services()

b1e88c3004b67c0e92fb9ec8d102752fe3ec9427
Maiker <gutierrezmaiker@gmail.com>
2026-04-11 21:55:46 -0600

    Add API launcher

--- src/api/wait_for_api.py
+++ src/api/wait_for_api.py
@@ -1,11 +1,12 @@
-import requests
+import urllib.request
 import time
 
 def wait_for_api():
     for _ in range(10):
         try:
-            requests.get("http://127.0.0.1:8000/")
-            return True
+            response = urllib.request.urlopen("http://127.0.0.1:8000/")
+            if response.status == 200:
+                return True
         except:
             time.sleep(1)
     return False

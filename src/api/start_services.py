75321e3cdb7409347f02245fda981690d8a09b94
Maiker <gutierrezmaiker@gmail.com>
2026-04-12 03:39:02 -0600

    Add API launcher

--- src/api/start_services.py
+++ src/api/start_services.py
@@ -1,4 +1,7 @@
 import subprocess
+import threading
+
+from src.main import app
 from src.utils.tunnel_data import tunnel_name
 from src.utils.PATHS import CLOUDFLARED_PATH
 from .wait_for_api import wait_for_api
@@ -6,11 +9,7 @@ from .run_api import run_api
 
 def start_services():
     print("Starting Services...")
-    subprocess.Popen(
-        ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL
-    )
+    threading.Thread(target=run_api, args=(app,), daemon=True).start()
 
     print("Waiting for API...")
     if not wait_for_api():

089a8249481763988a8f92fb640b558d155e8738
Maiker <gutierrezmaiker@gmail.com>
2026-04-12 00:39:11 -0600

    Add API launcher

--- src/setup/processes/create_tunnel.py
+++ src/setup/processes/create_tunnel.py
@@ -2,4 +2,7 @@ from src.utils.run_cmd import run_cmd
 from src.utils.PATHS import CLOUDFLARED_PATH
 
 def create_tunnel(tunnel_name):
-    run_cmd([CLOUDFLARED_PATH, "tunnel", "create", tunnel_name])
+    try:
+        run_cmd([CLOUDFLARED_PATH, "tunnel", "create", tunnel_name], True)
+    except:
+        print("Tunnel probablemente ya existe, continuando...")
--- src/utils/run_ps.py
+++ src/utils/run_ps.py
@@ -1,9 +1,10 @@
 import subprocess
 
-def run_ps(command: str):
+def run_ps(command: str, no_window: bool = False):
+
     return subprocess.run(
         ["powershell", "-Command", 
         command], 
         check=True,
-        creationflags=subprocess.CREATE_NO_WINDOW
+        creationflags=subprocess.CREATE_NO_WINDOW if no_window else 0
     )

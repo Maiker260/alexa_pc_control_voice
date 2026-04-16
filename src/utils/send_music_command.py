2ea5aafba700faa697d22b85c5f5c4f8868f60a1
Maiker <gutierrezmaiker@gmail.com>
2026-04-16 01:31:41 -0600

    Add feature to control the music volume

--- src/utils/send_music_command.py
+++ src/utils/send_music_command.py
@@ -1,16 +1,30 @@
 import json
 
 def send_music_comand(command):
-    pipe_path = r"\\.\pipe\mpvsocket"
+    import win32pipe
+    import win32file
 
-    message = json.dumps({"command": command})
+    pipe_name = r'\\.\pipe\mpvsocket'
+    message = json.dumps({"command": command}) + "\n"
 
     try:
-        with open(pipe_path, "w") as pipe:
-            pipe.write(message + "\n")
-            pipe.flush()
-        
+        win32pipe.WaitNamedPipe(pipe_name, 2000)
+
+        handle = win32file.CreateFile(
+            pipe_name,
+            win32file.GENERIC_WRITE,
+            0,
+            None,
+            win32file.OPEN_EXISTING,
+            0,
+            None
+        )
+
+        win32file.WriteFile(handle, message.encode())
+        handle.close()
+
         return True
+
     except Exception as e:
         print(f"Error sending command: {e}")
         return False

66b94f50c749cc913f8ccd755eb623ff245e0826
Maiker <gutierrezmaiker@gmail.com>
2026-04-16 02:05:01 -0600

    Add feature to control the music volume

--- src/utils/send_music_command.py
+++ src/utils/send_music_command.py
@@ -1,50 +1,21 @@
-# import json
-
-# def send_music_command(command):
-#     import win32pipe
-#     import win32file
-
-#     pipe_name = r'\\.\pipe\mpvsocket'
-#     message = json.dumps({"command": command}) + "\n"
-
-#     try:
-#         win32pipe.WaitNamedPipe(pipe_name, 2000)
-
-#         handle = win32file.CreateFile(
-#             pipe_name,
-#             win32file.GENERIC_WRITE,
-#             0,
-#             None,
-#             win32file.OPEN_EXISTING,
-#             0,
-#             None
-#         )
-
-#         win32file.WriteFile(handle, message.encode())
-#         handle.close()
-
-#         return True
-
-#     except Exception as e:
-#         print(f"Error sending command: {e}")
-#         return False
-
 import subprocess
+import json
 
 def send_music_command(command):
+    json_command = json.dumps({"command": command})
 
-    command = r'''
+    ps_script = f'''
     $pipe = new-object System.IO.Pipes.NamedPipeClientStream(".", "mpvsocket", [System.IO.Pipes.PipeDirection]::Out);
     $pipe.Connect();
     $sw = new-object System.IO.StreamWriter($pipe);
-    $sw.WriteLine('{ "command": ["add", "volume", 30] }');
+    $sw.WriteLine('{json_command}');
     $sw.Flush();
     $sw.Dispose();
     $pipe.Dispose()
     '''
 
     try:
-        subprocess.run(["powershell", "-Command", command])
+        subprocess.run(["powershell", "-Command", ps_script])
     except Exception as e:
         print(f"Error sending command: {e}")
         return False

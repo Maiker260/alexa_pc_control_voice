d6f25e69c8868dc2a2470aa0a64a2f197ab17364
Maiker <gutierrezmaiker@gmail.com>
2026-04-20 22:05:14 -0600

    Add media player installer/uninstaller

--- src/utils/ensure_admin.py
+++ src/utils/ensure_admin.py
@@ -6,6 +6,13 @@ def ensure_admin():
     if ctypes.windll.shell32.IsUserAnAdmin():
         return
 
+    ctypes.windll.user32.MessageBoxW(
+        0,
+        "The application will restart with administrator privileges.",
+        "Permission Required",
+        0x40
+    )
+
     params = " ".join([f'"{arg}"' for arg in sys.argv])
     
     ctypes.windll.shell32.ShellExecuteW(

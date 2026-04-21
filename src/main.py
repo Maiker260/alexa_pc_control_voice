ab8739b8d35a163e6b2596c40a1f55915ef5d765
Maiker <gutierrezmaiker@gmail.com>
2026-04-21 02:40:56 -0600

    Add the feature to delete the Cloudflare DNS entry in the uninstaller

--- src/main.py
+++ src/main.py
@@ -4,22 +4,22 @@ from src.handlers.handle_request import handle_request
 
 app = FastAPI()
 
-# @app.middleware("http")
-# async def block_unwanted(request: Request, call_next):
-#     if request.url.path != "/alexapc" or request.method != "POST":
-#         return Response(status_code=404)
+@app.middleware("http")
+async def block_unwanted(request: Request, call_next):
+    if request.url.path != "/alexapc" or request.method != "POST":
+        return Response(status_code=404)
     
-#     return await call_next(request)
+    return await call_next(request)
 
 @app.post("/alexapc")
 async def alexa(request: Request):
-    print(request.headers.get("x-api-key"))
-
     if not request.headers.get("cf-ray"):
         raise HTTPException(status_code=403, detail="Forbidden")
     
     API_KEY = get_api_key()
     # NEED TO DELETE THE PRINT
+    print(request.headers.get("x-api-key"))
+
 
     if request.headers.get("x-api-key") != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")

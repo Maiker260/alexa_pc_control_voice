a2dbe15092b268aa01cf0443e1a4ecc57d818670
Maiker <gutierrezmaiker@gmail.com>
2026-04-24 02:05:31 -0600

    Launcher runs in the system tray

--- main_launcher.py
+++ main_launcher.py
@@ -1,8 +1,17 @@
+import ctypes
+import sys
 import threading
 import multiprocessing
 
 from src.main_launcher.start_services import start_services
 from src.main_launcher.create_icon import create_icon
+
+# Avoid to run the app twice.
+mutex = ctypes.windll.kernel32.CreateMutexW(None, False, "AlexaPC_MUTEX")
+
+if ctypes.windll.kernel32.GetLastError() == 183:
+    print("App already running")
+    sys.exit(0)
 
 def main():
     threading.Thread(target=start_services, daemon=True).start()

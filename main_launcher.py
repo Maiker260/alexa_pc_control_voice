390bc61194ef6f8127c3927702d0d25a04fcd55e
Maiker <gutierrezmaiker@gmail.com>
2026-04-24 01:46:26 -0600

    Remove cmd window when installing the components

--- main_launcher.py
+++ main_launcher.py
@@ -1,10 +1,15 @@
 import threading
 import multiprocessing
+import os
+from datetime import datetime
 
 from src.main_launcher.start_services import start_services
 from src.main_launcher.create_icon import create_icon
 
 def main():
+    with open("debug.log", "a") as f:
+        f.write(f"{datetime.now()} PID={os.getpid()}\n")
+        
     threading.Thread(target=start_services, daemon=True).start()
     create_icon()
 

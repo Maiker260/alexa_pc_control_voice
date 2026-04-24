e79ed470262ae1fa3db2110116cd20f489a7c8bd
Maiker <gutierrezmaiker@gmail.com>
2026-04-24 02:24:26 -0600

    Launcher runs in the system tray

--- src/main_launcher/on_exit.py
+++ src/main_launcher/on_exit.py
@@ -1,15 +1,22 @@
 import os
-from .process_manager import process
+import psutil
+import src.main_launcher.process_manager as pm
+
+def kill_process_tree(pid):
+    try:
+        parent = psutil.Process(pid)
+        for child in parent.children(recursive=True):
+            child.kill()
+        parent.kill()
+    except Exception as e:
+        print("Kill error:", e)
 
 def on_exit(icon, item):
     print("Shutting down...")
 
-    if process:
-        try:
-            process.terminate()
-            process.wait(timeout=5)
-        except:
-            process.kill()
+    if pm.process:
+        print("Killing cloudflared PID:", pm.process.pid)
+        kill_process_tree(pm.process.pid)
 
     icon.stop()
     os._exit(0)

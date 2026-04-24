7d04263a84680782514c03dfbb5fffa3cc1adaaa
Maiker <gutierrezmaiker@gmail.com>
2026-04-23 23:18:07 -0600

    Launcher runs in the system tray

--- src/main_launcher/create_icon.py
+++ src/main_launcher/create_icon.py
@@ -4,7 +4,7 @@ from .on_exit import on_exit
 from src.utils.get_asset_path import get_asset_path
 
 def create_icon():
-    image = Image.open(get_asset_path("assets/icon.png"))
+    image = Image.open("assets/icon.png")
 
     icon = Icon(
         "AlexaPC",

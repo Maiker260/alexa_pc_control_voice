145ebca2fb84f022ff71a7fc5bcf0c1dbebc7f52
Maiker <gutierrezmaiker@gmail.com>
2026-04-28 00:26:54 -0600

    Stop processes before uninstalling the app

--- src/uninstall/processes/cloudflare/delete_tunnel_background.py
+++ src/uninstall/processes/cloudflare/delete_tunnel_background.py
@@ -1,15 +1,19 @@
 import subprocess
 import logging
-from src.utils.PATHS import CLOUDFLARED_PATH
+import os
+from src.utils.PATHS import CLOUDFLARED_PATH, USER_CONFIG_FILES_DIR
 from src.utils.tunnel_data import tunnel_name
 
 def delete_tunnel_background():
     try:
         cloudflared = f'"{CLOUDFLARED_PATH}"'
+
+        log_path = os.path.join(USER_CONFIG_FILES_DIR, "uninstall.log")
+        
         cmd = (
             f'timeout /t 50 >nul && '
             f'{cloudflared} tunnel delete {tunnel_name} '
-            f'>> "%APPDATA%\\AlexaPcVoiceControl\\uninstall.log" 2>&1'
+            f'>> "{log_path}" 2>&1'
         )
 
         subprocess.Popen(
@@ -18,6 +22,7 @@ def delete_tunnel_background():
             creationflags=subprocess.CREATE_NO_WINDOW
         )
 
-        logging.info("Scheduled tunnel deletion in background")
+        logging.info(f"Scheduled tunnel deletion in background -> {log_path}")
+        
     except Exception:
         logging.exception("Failed to schedule tunnel deletion")
